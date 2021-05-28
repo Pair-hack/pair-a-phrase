ac66c63921fa5d4b82ff001acf69a5e68d424c19
P-Hansen <philip.pt.hansen@gmail.com>
2021-05-28 14:58:50 -0600

    handle submit

--- pair-a-phrase-react/src/components/LoginForm.jsx
+++ pair-a-phrase-react/src/components/LoginForm.jsx
@@ -5,8 +5,20 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
+        const authObject = {"Project-ID": "8b2efd59-f1b9-458d-89d4-616c2e6e6713", "User-Name": username, "User-Secret": password}
+
+        try {
+            axios.get("https://api.chatengine.io/chats", { headers: authObject });
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+
+            window.location.reload();
+        } catch (err) {
+
+        }
     }
 
     return (
