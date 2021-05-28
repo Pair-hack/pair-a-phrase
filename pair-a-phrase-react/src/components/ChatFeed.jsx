e2af9f21d9805dd8b96f39fa59003ed26b75b9fd
Devin MacGillivray <devhmac@gmail.com>
2021-05-28 14:18:58 -0600

    building out render message list

--- pair-a-phrase-react/src/components/ChatFeed.jsx
+++ pair-a-phrase-react/src/components/ChatFeed.jsx
@@ -1,6 +1,54 @@
+import MessageForm from "./MessageForm";
+import UserMessage from "./UserMessage";
+import IncomingMessage from "./IncomingMessage";
+
 function ChatFeed(props) {
-  console.log(props);
-  return <div>Chat feed</div>;
+  const { chats, activeChat, userName, messages } = props;
+
+  //find current chat
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isUserMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isUserMessage ? <UserMessage /> : <IncomingMessage />}
+          </div>
+          <div
+            className="read--reciepts"
+            style={{
+              marginRight: isUserMessage ? "18px" : "0px",
+              marginLeft: isUserMessage ? "0px" : "68px",
+            }}
+          >
+            read-reciepts here
+          </div>
+        </div>
+      );
+    });
+  };
+
+  if (!chat) return "...Loading....";
+
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => `${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+    </div>
+  );
 }
 
 export default ChatFeed;
