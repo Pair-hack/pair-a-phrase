0213d5c3fd43c0840235a6d352414b7fa96e1a06
Devin MacGillivray <devhmac@gmail.com>
2021-05-28 15:26:13 -0600

    message form now send message

--- pair-a-phrase-react/src/components/MessageForm.jsx
+++ pair-a-phrase-react/src/components/MessageForm.jsx
@@ -1,4 +1,34 @@
-function MessageForm() {
-  return <div>Message Form</div>;
+import { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
+
+function MessageForm(props) {
+  const [inputVal, setInputVal] = useState("");
+  const { chatId, creds } = props;
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const text = inputVal.trim();
+    if (text.length > 0) sendMessage(creds, chatId, { text });
+
+    setInputVal("");
+  };
+  const onChangeHandler = (event) => {
+    setInputVal(event.target.value);
+
+    isTyping(props, chatId);
+  };
+
+  return (
+    <form className="message-form" onSubmit={submitHandler}>
+      <input
+        className="message-input"
+        placeholder="send a message"
+        value={inputVal}
+        onChange={onChangeHandler}
+        onSubmit={submitHandler}
+      />
+    </form>
+  );
 }
 export default MessageForm;
