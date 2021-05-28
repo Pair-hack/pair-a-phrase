25758d850a9ea2569dd7635c9ca955222368a18d
Devin MacGillivray <devhmac@gmail.com>
2021-05-28 14:46:31 -0600

    userMesage mostly complete

--- pair-a-phrase-react/src/components/UserMessage.jsx
+++ pair-a-phrase-react/src/components/UserMessage.jsx
@@ -1,4 +1,18 @@
-function UserMessage() {
-  return <div>Users message</div>;
+function UserMessage({ message }) {
+  // could include image check here
+
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#9DB750",
+      }}
+    >
+      {message.text}
+    </div>
+  );
 }
 export default UserMessage;
